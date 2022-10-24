5f99e01fe097f9bb0d0db4b9caca4ff900edb3ad
Ari N <an-nielson@hotmail.com>
2022-10-23 21:52:18 -0600

    multiple edits, updating/adding functionality

--- assets/script.js
+++ assets/script.js
@@ -2,42 +2,52 @@
 const today = moment();
 $('#currentDay').text(today.format('MMMM Do YYYY, h:mm a'));
 
+var currentHour = moment().format('hA');
+
 const containerEl = $('.container');
 
 const hours = [
-    '9AM',
-    '10AM',
-    '11AM',
-    '12PM',
-    '1PM',
-    '2PM',
-    '3PM',
-    '4PM',
+   // '9AM',
+   // '10AM',
+   // '11AM',
+   // '12PM',
+   // '1PM',
+   // '2PM',
+   // '3PM',
+   // '4PM',
     '5PM',
+    '6PM',
+    '7PM',
+    '8PM',
+    '9PM',
+    '10PM',
+    '11PM',
 ];
 
+// Create Planner Elements and depicts past, present, or future for textarea input
 for (var i = 0; i < hours.length; i++) {    
-    var hourlyDiv = $('<div>').addClass('row m-1');
+    var hourlyDiv = $('<div>').addClass('row');
     containerEl.append(hourlyDiv);
 
-    var hoursEl = $('<span>');
-    hoursEl.text(hours[i]);
-    hoursEl.addClass('col-1 text-right border-bottom');
+    var hoursEl = $('<span>').text(hours[i]).addClass('col-1 text-center hour pt-2').data({hours: [i]});
     hourlyDiv.append(hoursEl);
 
-    var textInput = $('<input>', {type: 'text'});
-    textInput.addClass('col-10 border-0 text-left bg-light');
+    var textInput = $('<input>', {type: 'text'}).addClass('col-10 textarea');
     hourlyDiv.append(textInput);
 
-    var saveBtn = $('<button>').text('🖫 Save');
-    saveBtn.addClass('btn btn-info col-1 text-center');
+    var saveBtn = $('<button>').text('🖫 Save').addClass('col-1 btn saveBtn');
     hourlyDiv.append(saveBtn);
+
+    if (currentHour == hours[i]) {
+        textInput.addClass('present');
+    } else if (currentHour != hours[i]) {
+        textInput.addClass('past');
+    } else {
+        textInput.addClass('future');
+    }
 }
 
-function presentHour() {  
-
-}
-
+// Saves textarea input data
 function saveTextInput() {
     
 }
